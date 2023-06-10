aa440bceb0bcc8135b12abdc06c3a6e39242daba
ZerNius <w.udit.shrivastava@gmail.com>
2023-06-10 18:18:23 +0530

    Added Sieve

---
+++ Mathematics/Sieve.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Sieve(int n)
+{
+    vector<bool> isPrime(n+1, true);
+
+    for(int i=2; i*i<=n; i++)
+    {
+        if(isPrime[i])
+        {
+            for(int j = i*i; j<=n; j = j+i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    } 
+
+    for(int i=2; i<n; i++)
+    {
+        if(isPrime[i])
+            cout<<i<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    Sieve(n);
+
+    return 0;
+}
