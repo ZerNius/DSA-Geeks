910af8ed33d82e30bf4f7f4fa709d142f5784166
ZerNius <w.udit.shrivastava@gmail.com>
2023-06-10 15:46:58 +0530

    Update Prime.cpp

--- Mathematics/Prime.cpp
+++ Mathematics/Prime.cpp
@@ -7,7 +7,7 @@ bool Prime(int num)
     if(num == 2 || num ==3 ) return true;
     if(num%2 == 0 || num%3 == 0) return false;
 
-    for(int i=5; i*i<num; i = i+6)
+    for(int i=5; i*i<=num; i = i+6)
     {
         if(num%i ==0 || num%(i+2) == 0)
             return false;
